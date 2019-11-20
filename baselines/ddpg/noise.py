0778273dfcf881082d8925cc958397e1a3b4469a
Ryan Rizzo <ryanrizzo66@gmail.com>
2019-11-20 21:24:19 +0100

    FInish other types of noise

--- baselines/ddpg/noise.py
+++ baselines/ddpg/noise.py
@@ -47,11 +47,73 @@ class AdaptiveParamNoiseSpec(object):
         fmt = 'AdaptiveParamNoiseSpec(initial_stddev={}, desired_action_stddev={}, adoption_coefficient={})'
         return fmt.format(self.initial_stddev, self.desired_action_stddev, self.adoption_coefficient)
 
+
 class ActionNoise(object):
+    """
+    The action noise base class
+    """
+    def reset(self):
+        """
+        call end of episode reset for the noise
+        """
+        pass
 
 
 class NormalActionNoise(ActionNoise):
+    """
+    Gaussian action noise
+
+    :param mean: (float) mean value of the noise
+    :param sigma: (float) the scale of the noise (standard deviation here)
+    """
+
+    def __init__(self, mean, sigma):
+        self._mean = mean
+        self._sigma = sigma
+
+    def __call__(self):
+        """
+        Create normal distribution according to desired mean and standard deviation
+        :return: norma distribution
+        """
+        return np.random.normal(self._mean, self._sigma)
+
+    def __repr(self):
+        return 'NormalActionNoise(mu={}, sigma={})'.format(self._mean, self._sigma)
 
 
 class OrnsteinUhlenbeckActionNoise(ActionNoise):
+    """
+    Ornstein Uhlenbeck action noise, designed tto approximate brownian motion with friction
 
+    :param mean: (float) the mean of the noise
+    :param sigma: (float) the scale of the noise
+    :param theta: (float) the rate of mean reversion
+    :param dt: (float) the timestep for the noise
+    :param initial_noise: ([float]) the initial value for the noise output, (if None: 0)
+    """
+
+    def __init__(self, mean, sigma, theta=.15, dt=1e-2, initial_noise=None):
+        self._mean = mean
+        self._sigma = sigma
+        self._theta = theta
+        self._dt = dt
+        self.initial_noise = initial_noise
+        self.noise_prev = None
+        self.reset()
+
+    def __call__(self):
+        noise = self.noise_prev + self._theta * (self._mean - self.noise_prev) * self._dt + \
+                self._sigma * np.sqrt(self._dt) * np.random.normal(size=self._mean.shape)
+
+        self.noise_prev = noise
+        return noise
+
+    def reset(self):
+        """
+        reset the Ornstein Uhlenbeck noise, to the initial position
+        """
+        self.noise_prev = self.initial_noise if self.initial_noise is not None else np.zeros_like(self._mu)
+
+    def __repr__(self):
+        return 'OrnsteinUhlenbeckActionNoise(mu={}, sigma={})'.format(self._mu, self._sigma)
